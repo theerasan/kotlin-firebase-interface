524fcd7f123c520826ba2651af4168f7f9afc932
Ta Tonthongkam <theerasan@eatigo.com>
2018-09-22 23:10:47 +0700

    Chang list type to array

--- src/main/kotlin/extention/Object.kt
+++ src/main/kotlin/extention/Object.kt
@@ -2,7 +2,7 @@ package extention
 
 external class Object {
     companion object {
-        fun keys(obj: Any): List<String>
-        fun <T> values(obj: Any): List<T>
+        fun keys(obj: Any): Array<String>
+        fun <T> values(obj: Any): Array<T>
     }
 }
